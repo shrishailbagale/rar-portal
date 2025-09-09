9e4240e561f416ccd9e51b0120cd9a150af88090
Shrishail Bagale <104571844+shrishailbagale@users.noreply.github.com>
2025-09-09 12:40:44 +0530

    script file added

---
+++ script.js
@@ -0,0 +1,10 @@
+﻿function toggleSidebar() {
+    const sidebar = document.getElementById('sidebar');
+    const main = document.getElementById('main');
+    if (window.innerWidth <= 768) {
+        sidebar.classList.toggle('show');
+    } else {
+        sidebar.classList.toggle('collapsed');
+        main.classList.toggle('collapsed');
+    }
+}
