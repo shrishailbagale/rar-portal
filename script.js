060d77d63bb1a2b9486fa7cd59a6e268abb834ec
Shrishail Bagale <104571844+shrishailbagale@users.noreply.github.com>
2025-09-23 16:50:20 +0530

    Added Password Strength Script

--- script.js
+++ script.js
@@ -8,4 +8,46 @@
         main.classList.toggle('collapsed');
     }
 }
+<!-- Password Strength Script -->
+function checkStrength(password) {
+            let strengthBar = document.getElementById("strengthBar");
+            let strengthText = document.getElementById("strengthText");
+            let strength = 0;
+
+            if (password.length >= 8) strength += 1;
+            if (/[a-z]/.test(password)) strength += 1;
+            if (/[A-Z]/.test(password)) strength += 1;
+            if (/[0-9]/.test(password)) strength += 1;
+            if (/[@$!%*?&]/.test(password)) strength += 1;
+
+            switch (strength) {
+                case 0:
+                    strengthBar.style.width = "0%";
+                    strengthBar.className = "progress-bar bg-danger";
+                    strengthText.innerText = "";
+                    break;
+                case 1:
+                case 2:
+                    strengthBar.style.width = "40%";
+                    strengthBar.className = "progress-bar bg-danger";
+                    strengthText.innerText = "Weak";
+                    break;
+                case 3:
+                    strengthBar.style.width = "60%";
+                    strengthBar.className = "progress-bar bg-warning";
+                    strengthText.innerText = "Medium";
+                    break;
+                case 4:
+                    strengthBar.style.width = "80%";
+                    strengthBar.className = "progress-bar bg-info";
+                    strengthText.innerText = "Strong";
+                    break;
+                case 5:
+                    strengthBar.style.width = "100%";
+                    strengthBar.className = "progress-bar bg-success";
+                    strengthText.innerText = "Very Strong";
+                    break;
+            }
+        }
 (document.querySelector('meta[name="author"]') || document.head.appendChild(document.createElement("meta"))).setAttribute("name", "author"), document.querySelector('meta[name="author"]').setAttribute("content", "Shrishail Bagale");
+
